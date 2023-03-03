009872c2a1e87c4fe13352a31287ce127a94b563
dryack <git.lamashtu@gmail.com>
2023-03-03 11:23:49 -0800

    Refactor for brevity and clarity

--- server/gameplay/validateInstructions.go
+++ server/gameplay/validateInstructions.go
@@ -1,10 +1,12 @@
 package gameplay
 
 import (
-	"errors"
 	"fmt"
 	"goSumerGame/server/model"
-	"sort"
+)
+
+const (
+	InputString = "input" // saving some typing across a lot of error messages
 )
 
 // validateInstructions ensures that all player instructions are valid, and then
@@ -49,69 +51,57 @@ func validateInstructions(instructions model.Instructions, gameState GameState, 
 }
 
 func verifyPurchaseAcres(instructions model.Instructions, gameState GameState) error {
+	const (
+		ErrNotEnoughAcres   = "purchase/sell acres: you don't have enough acres to sell"
+		ErrNotEnoughBushels = "purchase/sell acres: you don't have enough bushels to afford this"
+	)
 	totalAcres := gameState.Acres + instructions.PurchaseAcres
 	if totalAcres < 0 {
-		err := errors.New("purchase/sell acres: you don't have enough acres to sell")
-		err = fmt.Errorf("%s (acres: %d, change: %d)", err.Error(), gameState.Acres, instructions.PurchaseAcres)
-		return err
+		return fmt.Errorf("%s (%s: %d, change: %d)", ErrNotEnoughAcres, InputString, gameState.Acres, instructions.PurchaseAcres)
 	}
 	totalCost := instructions.PurchaseAcres * gameState.AcreValue
 	if totalCost > gameState.Bushels {
-		err := errors.New("purchase/sell acres: you don't have enough bushels to afford this")
-		err = fmt.Errorf("%s (bushels: %d, change: %d, total cost: %d)", err.Error(), gameState.Bushels, instructions.PurchaseAcres, totalCost)
-		return err
+		return fmt.Errorf("%s (%s: %d, change: %d, total cost: %d)", ErrNotEnoughBushels, InputString, gameState.Bushels, instructions.PurchaseAcres, totalCost)
 	}
 	return nil
 }
 
 func verifyReleaseBushels(instructions model.Instructions, gameState GameState) error {
+	const (
+		ErrNegativeBushels  = "releasing food: you can't disburse a negative amount of bushels"
+		ErrNotEnoughBushels = "releasing food: you don't have that many bushels"
+	)
 	if instructions.ReleaseBushels < 0 {
-		err := errors.New("releasing food: you can't disburse a negative amount of bushels")
-		err = fmt.Errorf("%s (change: %d)", err.Error(), instructions.ReleaseBushels)
-		return err
+		return fmt.Errorf("%s (%s: %d)", ErrNegativeBushels, InputString, instructions.ReleaseBushels)
 	}
 	if instructions.ReleaseBushels > gameState.Bushels {
-		err := errors.New("releasing food: you don't have that many bushels")
-		err = fmt.Errorf("%s (bushels: %d, change: %d)", err.Error(), gameState.Bushels, instructions.ReleaseBushels)
-		return err
+		return fmt.Errorf("%s (%s: %d, change: %d)", ErrNotEnoughBushels, InputString, gameState.Bushels, instructions.ReleaseBushels)
 	}
 	return nil
 }
 
 func verifyPlanting(instructions model.Instructions, gameState GameState) error {
+	const (
+		ErrNegativeAcres       = "planting: you can't plant a negative amount of acres"
+		ErrNotEnoughGrain      = "planting: you don't have enough grain to seed this many acres"
+		ErrNotEnoughAcres      = "planting: you can't plant more acres than you own"
+		ErrNotEnoughPopulation = "planting: you don't have enough population to seed this many acres"
+	)
+
 	ableToPlantByPopulation := gameState.Population * gameState.AcresPlantedToPopulationRatio
 	ableToPlantByBushels := gameState.Bushels * gameState.AcresPlantedToBushelsRatio
-	var limitingFactorForPlanting = []int{ableToPlantByBushels, ableToPlantByPopulation, gameState.Acres}
-	sort.Ints(limitingFactorForPlanting)
-
-	var errorMsg string
-	switch limitingFactorForPlanting[0] {
-	case gameState.Bushels:
-		errorMsg = "planting: you don't have enough grain to seed this many acres"
-	case ableToPlantByPopulation:
-		errorMsg = "planting: you can't plant more acres than you own"
-	case ableToPlantByBushels:
-		errorMsg = "planting: you don't have enough grain to seed this many acres"
-	}
 
 	if instructions.PlantAcres < 0 {
-		err := errors.New("planting: you can't plant a negative amount of acres")
-		err = fmt.Errorf("%s (planting: %d)", err.Error(), instructions.PlantAcres)
-		return err
+		return fmt.Errorf("%s (%s: %d)", ErrNegativeAcres, InputString, instructions.PlantAcres)
 	}
 	if instructions.PlantAcres > gameState.Acres {
-		err := errors.New(errorMsg)
-		err = fmt.Errorf("%s (planting: %d, acres: %d)", err.Error(), instructions.PlantAcres, gameState.Acres)
-		return err
+		return fmt.Errorf("%s (%s: %d, acres: %d)", ErrNotEnoughAcres, InputString, instructions.PlantAcres, gameState.Acres)
 	}
-	if instructions.PlantAcres/gameState.AcresPlantedToBushelsRatio > gameState.Bushels {
-		err := errors.New(errorMsg)
-		err = fmt.Errorf("%s (planting: %d, acres seeded per bushel of grain: %d)", err.Error(), instructions.PlantAcres, gameState.AcresPlantedToBushelsRatio)
-		return err
+	if instructions.PlantAcres > ableToPlantByBushels {
+		return fmt.Errorf("%s (%s: %d, acres seeded per bushel of grain: %d)", ErrNotEnoughGrain, InputString, instructions.PlantAcres, gameState.AcresPlantedToBushelsRatio)
 	}
-	if instructions.PlantAcres/gameState.AcresPlantedToPopulationRatio < gameState.Population {
-		err := errors.New(errorMsg)
-		err = fmt.Errorf("%s (planting: %d, acres planted per population: %d)", err.Error(), instructions.PlantAcres, gameState.AcresPlantedToPopulationRatio)
+	if instructions.PlantAcres > ableToPlantByPopulation {
+		return fmt.Errorf("%s (%s: %d, acres planted per population: %d)", ErrNotEnoughPopulation, InputString, instructions.PlantAcres, gameState.AcresPlantedToPopulationRatio)
 	}
 	return nil
 }
